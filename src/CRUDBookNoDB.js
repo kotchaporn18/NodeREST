a1b9ec9d6c7c16183c8fef93f59206e1b1e1db63
Kotchaporn18 <S6506021622116@email.kmutnb.ac.th>
2024-01-08 08:05:23 +0700

    save

--- src/CRUDBookNoDB.js
+++ src/CRUDBookNoDB.js
@@ -59,10 +59,4 @@ app.delete('/books/:id', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-
-
+app.listen(port, () => console.log(`Listen on port ${port}...`));
