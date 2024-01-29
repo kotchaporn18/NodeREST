d2200c18be2f12963ff26978e5a409782520ab2f
Kotchaporn18 <S6506021622116@email.kmutnb.ac.th>
2024-01-29 10:43:15 +0700

    save

--- numSplit.js
+++
@@ -1,8 +0,0 @@
-function numberSplit (num) {
-    return [Math.floor(num / 2), Math.ceil(num / 2)];
-}
-
-console.log(numberSplit(4));
-console.log(numberSplit(10));
-console.log(numberSplit(11));
-console.log(numberSplit(-9));
--- secretWord.js
+++
@@ -1,5 +0,0 @@
-function secretWord(str, n) {
-}
-
-console.log(secretWord("sadbpstcrdvaefikkgoenqrt", 5));
-console.log(secretWord("aheiayd", 3));    
